aa10fdd8750252d9e4e89a5beb6c93512ad9f4f7
Calebe <calebec055@gmail.com>
2025-05-23 21:41:08 +0100

    Integração do layout

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".showFormPalpiteDiario").addEventListener("click", ()=> {
+    const form = document.querySelector("#formPalpiteDiario")
+    form.classList.remove("hidden")
+})
